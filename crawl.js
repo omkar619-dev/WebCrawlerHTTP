14bba3e69a6eb187bf3c95afb3de4dc4d5f81cec
Omkar <omkarshendge619@gmail.com>
2024-03-26 10:28:09 +0530

    normalizeURL and tests

---
+++ crawl.js
@@ -0,0 +1,15 @@
+// the function you want to test.
+// write the tests for the function
+// write the meat of the function
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
+}
+
+module.exports = {
+  normalizeURL,
+};
